b87a8965485f273ddeb0eb3286b41ae8f5a823c2
GregoryDalessandro <gregorydalessandro942@gmail.com>
2018-08-14 22:17:04 -0400

    pass all tests for getElementsByClassName and fix the spec

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,27 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, node) {
+  //create an empty array targetClassNodes
+  var targetClassNodes = [];
+  //if node is undefined, then set node to document.body
+  if (node === undefined) {
+    node = document.body;
+  }
+  //if element.classList exists && element.classList equals className
+  if (node.classList && node.classList.contains(className)) {
+    //push the current nodes into targetClassNodes
+    targetClassNodes.push(node);
+  }
+  if (node.childNodes) {
+    node.childNodes.forEach( function(child) {
+      targetClassNodes = targetClassNodes.concat(getElementsByClassName(className, child));
+    });
+  }
+  //return targetClassNodes
+  return targetClassNodes;
 };
+
+
+
+
